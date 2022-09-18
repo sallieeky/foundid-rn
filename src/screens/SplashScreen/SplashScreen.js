bd19a386c766d60f7410698bdbf061eeb4b32f7c
sallieeky <sallieeky@gmail.com>
2022-09-18 16:53:33 +0800

    adding gmaps library and setup maps on mapscreen.js

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,6 +1,6 @@
 import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import API from '../../assets/api';
+import API from '../../config/api';
 
 const SplashScreen = () => {
   return (
