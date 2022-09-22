5d176185d77993d9bfe8046da02b2de59685278c
sallieeky <sallieeky@gmail.com>
2022-09-22 15:36:34 +0800

    set api to link from hosting

--- src/tabs/HomeTab/HomeTab.js
+++ src/tabs/HomeTab/HomeTab.js
@@ -1,7 +1,18 @@
 import {Button, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import API from '../../config/api';
 const HomeTab = ({navigation}) => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const data = await API.get('/tes');
+    setData(data.data);
+  };
+
   return (
     <View
       style={{
@@ -10,6 +21,9 @@ const HomeTab = ({navigation}) => {
         justifyContent: 'center',
       }}>
       <Text style={{fontFamily: 'Frijole-Regular', fontSize: 24}}>HomeTab</Text>
+      <Text style={{fontFamily: 'Frijole-Regular', fontSize: 16}}>
+        {data[0].pesan}
+      </Text>
       <Icon name="rocket" size={30} color="#900" />
       <Button
         title="Halaman Maps"
