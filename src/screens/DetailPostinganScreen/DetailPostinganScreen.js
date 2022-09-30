c23bb141a598b48409be5b72a13ebd616056611c
sallieeky <sallieeky@gmail.com>
2022-09-30 21:47:10 +0800

    adding some screen

---
+++ src/screens/DetailPostinganScreen/DetailPostinganScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const DetailPostinganScreen = () => {
+  return (
+    <View>
+      <Text>DetailPostinganScreen</Text>
+    </View>
+  );
+};
+
+export default DetailPostinganScreen;
